2f3eefeb2be59fc377b11f3a2acc1c3e7b3d2bf3
HunterRemex <github@remex.me>
2023-11-03 13:57:23 -0500

    ++ Add StringConstants record & coin 'prefab'

---
+++ Scripts/StringConstants.cs
@@ -0,0 +1,10 @@
+namespace CoinDashGaming.Scripts
+{
+	public record StringConstants
+	{
+		#region CONSTANTS
+		public const string GROUP_COINS = "coins";
+		public const string GROUP_OBSTACLES = "obstacles";
+		#endregion
+	}
+}
