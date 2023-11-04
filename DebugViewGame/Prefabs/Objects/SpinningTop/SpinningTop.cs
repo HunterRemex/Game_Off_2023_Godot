efdc00a48e706146a0ff86d9ffd915004f2cd3e9
HunterRemex <github@remex.me>
2023-11-04 15:18:30 -0500

    Add physics to spinning top, save CSG mesh
    + Save Spinning top  mesh to object

--- DebugViewGame/Prefabs/Objects/SpinningTop/SpinningTop.cs
+++ DebugViewGame/Prefabs/Objects/SpinningTop/SpinningTop.cs
@@ -1,7 +1,13 @@
 namespace CoinDashGaming.DebugViewGame.Prefabs.Objects.SpinningTop
 {
-	public class SpinningTop
+	using Scripts.FlawedBuilder;
+
+	public partial class SpinningTop : UnbalancedObject
 	{
-		
+		public override void _Ready()
+		{
+			base._Ready();
+			// TODO: Top has been spawned, create flaws, mat instances, etc...
+		}
 	}
 }
