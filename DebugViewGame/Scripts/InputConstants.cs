d756849938c3d8b0391e7ce72658d924321b5cbb
HunterRemex <github@remex.me>
2023-11-05 16:28:37 -0600

    ++ `InputConstants.cs`
    ++ Add InputConstants class to hold string IDs of input actions/chords

---
+++ DebugViewGame/Scripts/InputConstants.cs
@@ -0,0 +1,8 @@
+namespace CoinDashGaming.DebugViewGame.Scripts
+{
+	public record InputConstants
+	{
+		public const string INPUT_UI_ACCEPT = "ui_accept";
+		public const string INPUT_UI_SELECT = "ui_select";
+	}
+}
