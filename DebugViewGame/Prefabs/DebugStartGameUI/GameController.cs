e988bc42a3dd5666325449a233a66583ba31787a
HunterRemex <github@remex.me>
2023-11-10 12:49:58 -0600

    Add  more startup handling
    ++ TitleScreenController.cs: Script to interact w/GameController for starting game
    
    GameController.cs
    ~ Initialize singleton, test the debug render views

--- DebugViewGame/Prefabs/DebugStartGameUI/GameController.cs
+++ DebugViewGame/Prefabs/DebugStartGameUI/GameController.cs
@@ -4,14 +4,46 @@ namespace CoinDashGaming.DebugViewGame.Prefabs.DebugStartGameUI
 
 	public partial class GameController : Node
 	{
+		#region Singleton
+		public static GameController Instance { get; private set; }
+		#endregion
+
+		#region Public Methods
+		public void TransitionToGameplayMode()
+		{
+			// Disable Title Screen
+
+			// Enable Object Spawner
+
+			// Enable Gameplay UI~
+		}
+		#endregion
+
+		#region Godot Methods
 		public override void _Process(double delta)
 		{
 			base._Process(delta);
+
+			Viewport port = GetViewport();
+			if (port!= null &&
+				Time.GetTicksMsec() >> 3 == 0)
+			{
+				port.DebugDraw = (Viewport.DebugDrawEnum)(((int)port.DebugDraw + 1) % 26);
+				GD.Print(port.DebugDraw.ToString());
+			}
 		}
 
-		public override void _Ready()
+		public override void _EnterTree()
 		{
-			base._Ready();
+			if ( Instance == null )
+			{
+				Instance = this;
+			}
+			else
+			{
+				Free();
+			}
 		}
+		#endregion
 	}
 }
---
+++ DebugViewGame/Prefabs/DebugStartGameUI/TitleScreenController.cs
@@ -0,0 +1,24 @@
+namespace CoinDashGaming.DebugViewGame.Prefabs.DebugStartGameUI
+{
+	using Godot;
+
+	public partial class TitleScreenController : Node3D
+	{
+		#region Inspector-Set Fields
+		[Export]
+		private Button startGameButton;
+		#endregion
+
+		public override void _Ready()
+		{
+			base._Ready();
+			startGameButton.ButtonUp += StartGameButton_ButtonUp;
+		}
+
+		private void StartGameButton_ButtonUp()
+		{
+			GameController.Instance.TransitionToGameplayMode();
+
+		}
+	}
+}
