4a871d49b2f923563a43d53d66929756eb89cb31
HunterRemex <github@remex.me>
2023-11-12 16:08:13 -0600

    Make InputConstants.cs record abstract

--- DebugViewGame/Scripts/InputConstants.cs
+++ DebugViewGame/Scripts/InputConstants.cs
@@ -1,6 +1,6 @@
 namespace DebugViewGame.Meta
 {
-	public record InputConstants
+	public abstract record InputConstants
 	{
 		public const string INPUT_UI_ACCEPT = "ui_accept";
 		public const string INPUT_UI_SELECT = "ui_select";
